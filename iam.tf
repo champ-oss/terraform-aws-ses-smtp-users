717d48a2c1c3bd219d516b63a9607c5e03db3342
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-05 11:35:15 -0600

    ses smpt users requirements

--- iam.tf
+++ iam.tf
@@ -1,6 +1,12 @@
-data "aws_iam_policy" "this" {
-  arn = "arn:aws:iam::aws:policy/AmazonSesSendingAccess"
+data "aws_iam_policy_document" "smtp_user" {
+  statement {
+    actions = ["ses:SendRawEmail"]
+    resources = [
+      "*",
+    ]
+  }
 }
+
 
 resource "aws_iam_user" "smtp_user" {
   name = "ses-smtp-user-${random_string.identifier.result}"
@@ -11,7 +17,12 @@ resource "aws_iam_access_key" "smtp_user" {
   user = aws_iam_user.smtp_user.name
 }
 
+resource "aws_iam_policy" "this" {
+  name_prefix = "AmazonSesSendingAccess-"
+  policy      = data.aws_iam_policy_document.smtp_user.json
+}
+
 resource "aws_iam_user_policy_attachment" "this" {
   user       = aws_iam_user.smtp_user.name
-  policy_arn = data.aws_iam_policy.this.arn
+  policy_arn = aws_iam_policy.this.arn
 }
