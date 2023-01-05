20b2bd751fe40a203b383858a532502ea4902c12
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-05 14:39:24 -0600

    output password

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "smtp_username" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user#name"
   value       = aws_iam_user.smtp_user.name
 }
+
+output "smtp_password" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_access_key#ses_smtp_password_v4"
+  value       = aws_iam_access_key.smtp_user.ses_smtp_password_v4
+  sensitive   = true
+}
