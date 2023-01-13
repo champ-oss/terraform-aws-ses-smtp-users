16f7459841043af28c0875976460ebbb8fde4005
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-13 16:18:09 -0600

    updating username id

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "smtp_username" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user#name"
-  value       = aws_iam_user.smtp_user.name
+  value       = aws_iam_user.smtp_user.id
 }
 
 output "smtp_password" {
