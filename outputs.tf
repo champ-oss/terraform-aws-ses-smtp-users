988809768078f071bd7e6b53e8bd4529eb594c0f
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-18 09:47:14 -0600

    adding secret for output for testing

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,8 @@ output "smtp_key" {
   value       = aws_iam_access_key.smtp_user.id
 }
 
-output "smtp_password" {
+output "smtp_secret" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_access_key#ses_smtp_password_v4"
-  value       = aws_iam_access_key.smtp_user.ses_smtp_password_v4
+  value       = aws_iam_access_key.smtp_user.secret
   sensitive   = true
 }
--- ssm.tf
+++ ssm.tf
@@ -2,6 +2,6 @@ resource "aws_ssm_parameter" "smtp_user" {
   name        = "ses-smtp-user-${random_string.identifier.result}"
   description = "smtp user password"
   type        = "SecureString"
-  value       = aws_iam_access_key.smtp_user.ses_smtp_password_v4
+  value       = aws_iam_access_key.smtp_user.secret
   tags        = merge(local.tags, var.tags)
 }
